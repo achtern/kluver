360bdd50f66868d4062ab9790098e72f3511b6a8
Christian Gärtner <chrisdagardner@gmail.com>
2015-06-28 22:10:08 +0200

    Added a util method to convert int Pos to linenumber

---
+++ util/util.go
@@ -0,0 +1,16 @@
+// Copyright 2015 Christian Gärtner. All rights reserved.
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file.
+
+package util
+
+import (
+	"strings"
+	"strconv"
+)
+
+func GetLineFromPos(input, pos string) int {
+	upper, _ := strconv.Atoi(pos)
+	// add one, since line numbers do not start at zero
+	return strings.Count(input[0:upper], "\n") + 1
+}
